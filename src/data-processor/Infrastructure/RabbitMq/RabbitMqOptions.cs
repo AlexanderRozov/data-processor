26ce7082692358c3aba1b9685ca5b015ba60efaf
alexander.rozov <alexander.rozov>
2025-11-27 16:17:34 +0400

    Oprtions and consumer service

---
+++ src/data-processor/Infrastructure/RabbitMq/RabbitMqOptions.cs
@@ -0,0 +1,13 @@
+namespace DataProcessor.Infrastructure.RabbitMq;
+
+public class RabbitMqOptions
+{
+    public string HostName { get; set; } = "localhost";
+    public int Port { get; set; } = 5672;
+    public string UserName { get; set; } = "guest";
+    public string Password { get; set; } = "guest";
+    public string QueueName { get; set; } = "events";
+}
+
+
+
